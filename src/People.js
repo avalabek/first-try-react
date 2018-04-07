0c8448f982efda5f557d73c3c5b4bf4aefa61990
Andreya <avalabek@yahoo.com>
2018-04-07 16:02:42 -0700

    clean up files

--- src/People.js
+++ src/People.js
@@ -4,27 +4,28 @@ import Input from "./Input";
 
 // import Listitem from "./Listitem";
 // The below takes the array and maps them into list items
- const people = ["Does", "This", "Work"];
+
+const people = ["Does", "This", "Work"];
+ 
 // the below doesn't work
 // const people = {this.state.friends}
-const peopleLis = people.map(person =>
-  <li>{person}</li>
-);
+const peopleLis = people.map(person => (
+  <li className="list-group-item">{person}</li>
+));
 // By extending the React.Component class, Counter inherits functionality from it
 class People extends React.Component {
 
   render() {
-    return (
-      <div>
-      <h3>Demonstrate mapping over an array and rendering as list items</h3>  
-      <ul>{peopleLis}</ul>
-      <h3>Demonstrate entering input and having the header render instantly</h3>
-      <Input />
-      </div>
-      
-
-
-    )
+    return <div>
+        <h3>
+          Demonstrate mapping over an array and rendering as list items
+        </h3>
+        <ul >{peopleLis}</ul>
+        <h3>
+          Demonstrate entering input and having the header render instantly
+        </h3>
+        <Input />
+      </div>;
   }
 }
 
